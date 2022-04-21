8d9dae7b216680c7c76242854001fb38bd18874c
Pawan Kumar <surpawan@gmail.com>
2022-04-21 21:35:10 +0530

    Added Logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,79 @@ const App = () => {
   const [loanAmount, setLoanAmount] = useState(20000)
   const [loanTerms, setLoanTerms] = useState(4)
   const [repaymentTerms, setRepaymentTerms] = useState('weekly')
+  const [totalRepayments, setTotalRepayments] = useState(0)
+  const [emi, setEmi] = useState(0)
 
-  const formattedLoanAmount = Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumSignificantDigits: 5,
-  }).format(loanAmount)
+  const calculateLoanTerms = () => {
+    switch (repaymentTerms) {
+      case 'weekly':
+        return loanTerms * 52
+      case 'fortnightly':
+        return loanTerms * 26
+      case 'monthly':
+        return loanTerms * 12
+      default:
+        break
+    }
+  }
+
+  // Accrued Amount = A = P(1 + r/n)nt
+  const calculateEMI = () => {
+    const yearlyInterestRate = interestRates[loanTerms - 1] / 100
+    const loanTerm = calculateLoanTerms()
+
+    const accruedAmount = parseInt(
+      loanAmount * (1 + yearlyInterestRate / loanTerm) ** (loanTerm * loanTerms)
+    )
+
+    setEmi(formattedAmount(parseInt(accruedAmount / loanTerm)))
+    setTotalRepayments(formattedAmount(accruedAmount))
+  }
+
+  const formattedAmount = (amount) => {
+    return Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumSignificantDigits: 5,
+    }).format(amount)
+  }
 
   useEffect(() => {
-    // Calculate EMI and Total Repayment
-  }, [loanAmount, loanTerms, repaymentTerms])
+    calculateEMI()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="App text-black bg-white h-full relative">
-      <div className="flex flex-col md:flex-row h-screen">
-        <div className="h-1/2 md:w-full md:h-full flex md:flex-row">
-          <div className="bg-primary h-full w-1/4 invisible md:visible" />
+      <div className="flex flex-col lg:flex-row h-screen">
+        <div className="h-1/2 md:h-2/3 lg:w-full lg:h-full flex lg:flex-row">
+          <div className="bg-primary h-full w-1/4 invisible lg:visible" />
           <img
             src="/loan-calc.png"
-            className="h-auto w-full md:w-3/4 object-cover"
+            className="h-auto w-full lg:w-3/4 object-cover"
             alt="loan calc"
           />
         </div>
-        <div className="px-4 md:hidden flex w-full justify-center h-1/2"></div>
+        <div className="px-4 lg:hidden flex w-full justify-center h-1/2 md:h-1/3"></div>
       </div>
 
       {/* Card */}
-      <div className="md:bg-white md:shadow-lg top-80 bottom-0 md:rounded-lg w-full p-4 md:p-8 mx-0 absolute md:max-w-xl md:inset-y-0 md:inset-x-1 md:m-10">
-        <div className="bg-white p-4 shadow-lg rounded-lg md:bg-transparent md:p-0 md:shadow-none md:rounded-none">
+      <div className="lg:bg-white lg:shadow-lg top-80 md:top-none md:inset-y-1/2 bottom-0 lg:rounded-lg w-full h-fit p-4 lg:p-8 mx-0 absolute lg:max-w-xl lg:inset-y-0 lg:inset-x-1 lg:m-10">
+        <div className="bg-white p-4 shadow-lg rounded-lg lg:bg-transparent lg:p-0 lg:shadow-none lg:rounded-none">
           {/* Card heading */}
-          <div className="text-2xl font-semibold mb-8 md:mb-16">
+          <div className="text-2xl font-semibold mb-8 lg:mb-16">
             Your interest calculator
           </div>
           {/* Card Body */}
           <div className="w-full flex flex-col">
             {/* Loan Amount */}
-            <div className="mb-6 md:mb-10">
+            <div className="mb-6 lg:mb-10">
               <div className="flex justify-between mb-2">
                 <label className="font-semibold" htmlFor="loanAmount">
                   Loan Amount
                 </label>
                 <div className="text-primary font-semibold">
-                  {formattedLoanAmount}
+                  {formattedAmount(loanAmount)}
                 </div>
               </div>
               <input
@@ -56,12 +87,12 @@ const App = () => {
                 type="range"
                 min="1000"
                 max="35000"
-                onChange={(e) => setLoanAmount(e.target.value)}
+                onChange={(e) => setLoanAmount(parseInt(e.target.value))}
                 value={loanAmount}
               />
             </div>
             {/* Loan Terms */}
-            <div className="mb-6 md:mb-10">
+            <div className="mb-6 lg:mb-10">
               <div className="flex justify-between mb-2">
                 <label className="font-semibold" htmlFor="loanTerms">
                   Loan Terms
@@ -70,7 +101,7 @@ const App = () => {
               <select
                 name="loanTerms"
                 className="w-full border rounded py-2 border-gray"
-                onChange={(e) => setLoanTerms(e.target.value)}
+                onChange={(e) => setLoanTerms(parseInt(e.target.value))}
                 value={loanTerms}
               >
                 <option value="1">1 Year</option>
@@ -80,7 +111,7 @@ const App = () => {
               </select>
             </div>
             {/* Repayment Terms */}
-            <div className="mb-6 md:mb-10">
+            <div className="mb-6 lg:mb-10">
               <div className="flex justify-between mb-2">
                 <label className="font-semibold" htmlFor="repaymentTerms">
                   Repayment Terms
@@ -95,24 +126,26 @@ const App = () => {
                 <option value="weekly">Weekly</option>
                 <option value="fortnightly">Fortnightly</option>
                 <option value="monthly">Monthly</option>
-                <option value="quarterly">Quarterly</option>
               </select>
             </div>
             {/* Loan Summary */}
             <div className="flex w-full font-semibold">
               <div className="w-1/2 flex flex-col">
                 <div className="capitalize">{`${repaymentTerms} repayments`}</div>
-                <div className="text-xl">$99.62</div>
+                <div className="text-xl">{emi}</div>
               </div>
               <div className="w-1/2 flex flex-col">
                 <div>Total Repayments</div>
-                <div className="text-xl">$20,720</div>
+                <div className="text-xl">{totalRepayments}</div>
               </div>
             </div>
           </div>
           {/* Card Footer */}
-          <div className="mt-6 md:mt-16">
-            <button className="bg-primary rounded-lg text-white px-2 py-4 w-full text-center">
+          <div className="mt-6 lg:mt-16">
+            <button
+              onClick={calculateEMI}
+              className="bg-primary rounded-lg text-white px-2 py-4 w-full text-center"
+            >
               Calculate repayments
             </button>
           </div>
